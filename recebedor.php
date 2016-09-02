bab906f2d4d160df16cadc4b2f8cafd9266a7573
alexsanderBenny <alexsanderbenicio.m.a@gmail.com>
2016-09-02 11:52:56 -0300

    teste2

--- recebedor.php
+++ recebedor.php
@@ -1,6 +1,6 @@
 <?php
 
-    if (isset($_POST['email']) && !empty($_POST['email'])){
+    if (isset($_POST['email']) && empty($_POST['email']) == false){
 
         $email = $_POST['email'];
         echo "você é demais!asa!!!! ".$email;
